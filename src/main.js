8569fdb9dd2a979744c3ed22a1ce2734736dd40b
阿夢 <ikaroinory233@gmail.com>
2022-10-18 21:24:31 +0800

    feat: ElementPlusIcons

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import store from "@/store";
 
 import I18nInstance from "@/plugins/I18n";
 import ElementPlusInstance from "@/plugins/ElementPlus";
+import ElementPlusIconsInstance from "@/plugins/ElementPlusIcons"
 import axios from "@/plugins/Axios";
 
 const app = createApp(App);
 
 I18nInstance(app);
 ElementPlusInstance(app);
+ElementPlusIconsInstance(app);
 
 app.use(store);
 app.use(router);
